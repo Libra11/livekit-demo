33b47307463b263c65b2d7fd8f805cb592737ef3
wudidada <hackerjiang@gmail.com>
2024-04-01 00:45:48 +0800

    fix replay size

--- liveKit-client/src/views/StartReplayView.vue
+++ liveKit-client/src/views/StartReplayView.vue
@@ -10,7 +10,7 @@
 		<div class="w-80">
 			<el-form ref="form" :model="formData" label-width="0">
 				<el-form-item>
-					<el-input v-model="formData.url" placeholder="请输入用户名"></el-input>
+					<el-input v-model="formData.url" placeholder="请输入url"></el-input>
 				</el-form-item>
 				<el-form-item>
 					<el-button type="primary" @click="start">开始观看</el-button>
